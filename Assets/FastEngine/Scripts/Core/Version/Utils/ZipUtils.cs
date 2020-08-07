f01722d68f025db513954396a824f4f7769e6b52
reah <1157434689@qq.com>
2020-08-07 16:53:04 +0800

    添加dll

---
+++ Assets/FastEngine/Scripts/Core/Version/Utils/ZipUtils.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace FastEngine
+{
+
+    public static class ZipUtils
+    {
+        #region ZipCallback
+        public abstract class ZipCallback
+        {
+            /// <summary>
+            ///  压缩单个文件或文件夹前执行的回调
+            /// </summary>
+            /// <param name="_entyr"></param>
+            /// <returns>如果返回true,则压缩文件或文件夹，反之则不压缩文件或文件夹</returns>
+            public virtual bool OnPreZip(ZipEntry _entry)
+            {
+                return true;
+            }
+
+            /// <summary>
+            /// 压缩单个文件或文件夹后执行的回调
+            /// </summary>
+            /// <param name="_entry"></param>
+            public virtual void OnPostZip(ZipEntry _entry) { }
+
+            /// <summary>
+            /// 压缩执行完毕后的回调
+            /// </summary>
+            /// <param name="_result"></param>
+            public virtual void OnFinished(bool _result) { }
+        }
+        #endregion
+
+
+    }
+}
+
