a78d03001c150629ef8897140d5f247e5688290e
reah <1157434689@qq.com>
2020-09-01 21:01:59 +0800

    2020/09/01 提交

--- Assets/FastEngine/Scripts/Core/Version/Utils/ZipUtils.cs
+++ Assets/FastEngine/Scripts/Core/Version/Utils/ZipUtils.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using ICSharpCode.SharpZipLib.Zip;
 
@@ -35,6 +37,110 @@ namespace FastEngine
         }
         #endregion
 
+        #region UnzipCallback
+        public abstract class UnzipCallback
+        {
+            /// <summary>
+            /// 解压单个文件或文件夹前执行的回调
+            /// </summary>
+            /// <param name="_entry"></param>
+            /// <returns>如果返回true，则压缩文件或文件夹，反之则不压缩文件或文件夹</returns>
+            public virtual bool OnPreUnzip(ZipEntry _entry)
+            {
+                return true;
+            }
+
+            /// <summary>
+            /// 解压单个文件或文件夹后执行的回调
+            /// </summary>
+            /// <param name="_entry"></param>
+            public virtual void OnPostUnzip(ZipEntry _entry) { }
+
+            /// <summary>
+            /// 解压执行完毕后的回调
+            /// </summary>
+            /// <param name="_result">true表示解压成功，false表示解压失败</param>
+            public virtual void OnFinished(bool _result) { }
+        }
+        #endregion
+
+        public static bool Zip(string[] fileArray, string[] parentbArray, string _outputPathName,string _possword = null, ZipCallback _zipCallback = null)
+        {
+            if (fileArray == null || parentbArray == null || fileArray.Length != fileArray.Length ||
+                string.IsNullOrEmpty(_outputPathName))
+            {
+                if(_zipCallback !=null)
+                    _zipCallback.OnFinished(false);
+                return false;
+            }
+
+            ZipOutputStream zipOutputStream = new ZipOutputStream(File.Create(_outputPathName));
+            // 压缩质量和压缩速度的平衡点
+            zipOutputStream.SetLevel(6);
+            if (!string.IsNullOrEmpty(_possword))
+                zipOutputStream.Password = _possword;
+
+            for (int index = 0; index < fileArray.Length; index++)
+            {
+                bool result = false;
+                string fileOrDirectory = fileArray[index];
+                if (File.Exists((fileOrDirectory)))
+                {
+                    result = ZipFile()                }
+                    
+            }
+            if (_zipCallback != null)
+                _zipCallback.OnFinished(true);
+            return true;
+        }
+
+        private static bool ZipFile(string _filePathName, string _parentRelPath, ZipOutputStream _zipOutputStream,
+            ZipCallback _zipCallback = null)
+        {
+            //Crc32 crc32 = new Crc32();
+            ZipEntry entry = null;
+            FileStream fileStream = null;
+            try
+            {
+                string entryName = _parentRelPath + '/' + Path.GetFileName(_filePathName);
+                entry = new ZipEntry(entryName);
+                entry.DateTime = System.DateTime.Now;
+
+                if ((_zipCallback != null) && _zipCallback.OnPreZip(entry))
+                    return true; // 过滤
+
+                fileStream = File.OpenRead(_filePathName);
+                byte[] buffer = new byte[fileStream.Length];
+                fileStream.Read(buffer, 0, buffer.Length);
+                fileStream.Close();
+
+                entry.Size = buffer.Length;
+
+                //crc32.Reset();
+                //crc32.Update(buffer);
+                //entry.Crc = crc32.Value;
+
+                _zipOutputStream.PutNextEntry(entry);
+                _zipOutputStream.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("[ZipUtility.ZipFile]: Failled File: " + _filePathName);
+                Debug.LogError("[ZipUtility.ZipFile]: " + e.ToString());
+                return false;
+            }
+            finally
+            {
+                if (fileStream != null)
+                {
+                    fileStream.Close();
+                    fileStream.Dispose();
+                }
+            }
+            if(_zipCallback != null)
+                _zipCallback.OnPostZip(entry);
+            return true;
+        }
 
     }
 }
