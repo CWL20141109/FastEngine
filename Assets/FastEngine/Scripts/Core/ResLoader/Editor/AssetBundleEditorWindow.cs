bdf37b3a2beb782c36464b1da1c82bfa4be9bc2a
reah <1157434689@qq.com>
2020-09-11 18:54:51 +0800

    2020/09/11

---
+++ Assets/FastEngine/Scripts/Core/ResLoader/Editor/AssetBundleEditorWindow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FastEngine.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace FastEngine.Editor.AssetBundle
+{
+    public class AssetBundleEditorWindow : FastEditorWindow
+    {
+        private static AssetBundleConfig assetBundleConfig;
+
+        private Vector3 _scrollPosition;
+        private int _bar = 0;
+        private string[] _barStrs = new[] { "AssetBundle", "Copy Source" };
+
+        protected override void OnInitialize()
+        {
+            titleContent.text = "AssetBundle 配置编辑器";
+            assetBundleConfig = Config.ReadEditorDirectory<AssetBundleConfig>();
+        }
+
+        void OnGUI()
+        {
+            EditorGUILayout.BeginHorizontal("box");
+            if (GUILayout.Button("Save"))
+            {
+                assetBundleConfig.Save<AssetBundleConfig>();
+                AssetDatabase.Refresh();
+            }
+
+            if (GUILayout.Button(EditorGUIUtility.IconContent("Toolbar Plur")))
+            {
+                if (_bar == 0)
+                {
+                    var pack = new Pack();
+                    pack.editorShow = true;
+                    assetBundleConfig.packs.Add(pack);
+                }
+                else if(_bar == 1)
+                {
+                    var source = new Source();
+                    source.editorShow = true;
+                    assetBundleConfig.sources.Add(source);
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+
+            _bar = GUILayout.Toolbar(_bar, _barStrs);
+
+            EditorGUILayout.BeginVertical("box");
+        }
+    }
+
+}
+
